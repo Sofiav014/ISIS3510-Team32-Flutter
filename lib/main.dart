0eeeeba26380fe1737df4a0873ab42fe8290ec97
Sofia Velasquez Marin <79645972+Sofiav014@users.noreply.github.com>
2025-03-03 21:06:10 -0500

    fix: add WidgetsFlutterBinding.ensureInitialized()

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'core/firebase_options.dart';
 
 Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
