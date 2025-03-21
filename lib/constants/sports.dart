13b50d4ca9aaffc0d7523a8cc7d8bf75adeb796e
Sofia Velasquez Marin <s.velasquezm2@uniandes.edu.co>
2025-03-21 10:02:34 -0500

    refactor: Update sport IDs to use sport names for better clarity

--- lib/constants/sports.dart
+++ lib/constants/sports.dart
@@ -2,19 +2,19 @@ import 'package:isis3510_team32_flutter/models/sport_model.dart';
 
 Map<String, SportModel> initiationSports = {
   "basketball": SportModel(
-      id: "1",
+      id: "basketball",
       name: "Basketball",
       logo: "assets/icons/initiation/basketball-logo.svg"),
   "football": SportModel(
-      id: "2",
+      id: "football",
       name: "Football",
       logo: "assets/icons/initiation/football-logo.svg"),
   "volleyball": SportModel(
-      id: "3",
+      id: "volleyball",
       name: "Volleyball",
       logo: "assets/icons/initiation/volleyball-logo.svg"),
   "tennis": SportModel(
-      id: "4",
+      id: "tennis",
       name: "Tennis",
       logo: "assets/icons/initiation/tennis-logo.svg"),
 };
