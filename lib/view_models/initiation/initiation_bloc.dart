d72b6854b02d8a0552f632518e592bfa9b45f16c
Nicolas-Saavedra <personal@nicolassaavedra.com>
2025-04-16 12:49:25 -0500

    feat: hydrate initiation, add connectivity to DI

--- lib/view_models/initiation/initiation_bloc.dart
+++ lib/view_models/initiation/initiation_bloc.dart
@@ -1,8 +1,9 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:isis3510_team32_flutter/models/sport_model.dart';
 import 'package:isis3510_team32_flutter/view_models/initiation/initiation_event.dart';
 import 'package:isis3510_team32_flutter/view_models/initiation/initiation_state.dart';
 
-class InitiationBloc extends Bloc<InitiationEvent, InitiationState> {
+class InitiationBloc extends HydratedBloc<InitiationEvent, InitiationState> {
   InitiationBloc() : super(InitiationState()) {
     on<InitiationNameEvent>((event, emit) {
       emit(state.copyWith(name: event.name));
@@ -35,5 +36,28 @@ class InitiationBloc extends Bloc<InitiationEvent, InitiationState> {
         emit(state.copyWith(currentStep: state.currentStep - 1));
       }
     });
+  }
+
+  @override
+  InitiationState? fromJson(Map<String, dynamic> json) {
+    return InitiationState(
+      name: json['name'] as String?,
+      birthDate: json['birthDate']
+          as DateTime?, // or DateTime.parse if it's a DateTime
+      gender: json['gender'] as String?,
+      sportsLiked: List<SportModel>.from(json['sportsLiked'] ?? []),
+      currentStep: json['currentStep'] as int,
+    );
+  }
+
+  @override
+  Map<String, dynamic>? toJson(InitiationState state) {
+    return {
+      'name': state.name,
+      'birthDate': state.birthDate,
+      'gender': state.gender,
+      'sportsLiked': state.sportsLiked,
+      'currentStep': state.currentStep,
+    };
   }
 }
