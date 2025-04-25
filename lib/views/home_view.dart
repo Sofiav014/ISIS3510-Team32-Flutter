250f8bc5ec6c837106f0904640eb41672437ded5
Sofia Velasquez Marin <s.velasquezm2@uniandes.edu.co>
2025-04-24 22:26:03 -0500

    fix: correct import path for connectivity_repository in home_view.dart

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -2,7 +2,7 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:isis3510_team32_flutter/core/app_colors.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:isis3510_team32_flutter/repositories/connectivity_repository.dart';
+import 'package:isis3510_team32_flutter/models/repositories/connectivity_repository.dart';
 import 'package:isis3510_team32_flutter/widgets/bottom_navigation_widget.dart';
 import 'package:isis3510_team32_flutter/models/data_models/booking_model.dart';
 import 'package:isis3510_team32_flutter/view_models/home/home_bloc.dart';
